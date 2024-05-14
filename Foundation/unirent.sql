cc8c174bfdf5905769c442af7433380cd4ff4297
Elisabetta Mecozzi <elisabetta.mecozzi0@gmail.com>
2024-05-14 15:51:39 +0200

    Aggiunta cartella Foundation con all'interno il database

---
+++ Foundation/unirent.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 14, 2024 alle 15:45
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `unirent`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `accomodation`
+--
+
+CREATE TABLE `accomodation` (
+  `id` int(11) NOT NULL,
+  `photo` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `address` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `start` date NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `deposit` double DEFAULT NULL,
+  `visitAvaiability` int(11) NOT NULL,
+  `visitDuration` time NOT NULL,
+  `man` tinyint(1) NOT NULL,
+  `woman` tinyint(1) NOT NULL,
+  `pets` tinyint(1) NOT NULL,
+  `smokers` tinyint(1) NOT NULL,
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `streetName` varchar(50) NOT NULL,
+  `houseNumber` int(11) NOT NULL,
+  `postalCode` int(11) NOT NULL,
+  `city` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `day`
+--
+
+CREATE TABLE `day` (
+  `id` int(11) NOT NULL,
+  `day` varchar(20) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `owner`
+--
+
+CREATE TABLE `owner` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(20) NOT NULL,
+  `picture` blob DEFAULT NULL,
+  `email` varchar(40) NOT NULL,
+  `phoneNumber` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `photo`
+--
+
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL,
+  `photo` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(20) NOT NULL,
+  `picture` blob DEFAULT NULL,
+  `universityMail` varchar(40) NOT NULL,
+  `courseDuration` int(1) NOT NULL,
+  `immatricolationYear` int(4) NOT NULL,
+  `birthDate` date NOT NULL,
+  `sex` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `time`
+--
+
+CREATE TABLE `time` (
+  `id` int(11) NOT NULL,
+  `hour` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `accomodation`
+--
+ALTER TABLE `accomodation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `photo` (`photo`),
+  ADD KEY `address` (`address`),
+  ADD KEY `visitAvaiability` (`visitAvaiability`),
+  ADD KEY `owner` (`owner`);
+
+--
+-- Indici per le tabelle `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email_2` (`email`);
+
+--
+-- Indici per le tabelle `day`
+--
+ALTER TABLE `day`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `day` (`day`),
+  ADD KEY `time` (`time`);
+
+--
+-- Indici per le tabelle `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phoneNumber` (`phoneNumber`);
+
+--
+-- Indici per le tabelle `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `universityMail` (`universityMail`);
+
+--
+-- Indici per le tabelle `time`
+--
+ALTER TABLE `time`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hour` (`hour`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `accomodation`
+--
+ALTER TABLE `accomodation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `day`
+--
+ALTER TABLE `day`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `time`
+--
+ALTER TABLE `time`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `accomodation`
+--
+ALTER TABLE `accomodation`
+  ADD CONSTRAINT `accomodation_ibfk_1` FOREIGN KEY (`photo`) REFERENCES `photo` (`id`),
+  ADD CONSTRAINT `accomodation_ibfk_2` FOREIGN KEY (`address`) REFERENCES `address` (`id`),
+  ADD CONSTRAINT `accomodation_ibfk_3` FOREIGN KEY (`visitAvaiability`) REFERENCES `day` (`id`),
+  ADD CONSTRAINT `accomodation_ibfk_4` FOREIGN KEY (`owner`) REFERENCES `owner` (`id`);
+
+--
+-- Limiti per la tabella `day`
+--
+ALTER TABLE `day`
+  ADD CONSTRAINT `day_ibfk_1` FOREIGN KEY (`time`) REFERENCES `time` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
