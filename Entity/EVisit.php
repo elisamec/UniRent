cb7735a7f5184775fd9f2d46cad4606bc6125c0b
Nadia Muzyka <muzykanadia0@gmail.com>
2024-05-22 17:06:53 +0200

    EVisit updated

--- Entity/EVisit.php
+++ Entity/EVisit.php
@@ -4,10 +4,7 @@ class EAccommodation
 {
     private int $idVisit;
     private DateTime $day;
-    //Perchè non includere time dentro day? 
-    //La classe DateTime contiene sia una data che un orario
-
-    //Stessa cosa qui per le chiavi esterne..
-    //Alla fine dobbiamo avere un array dentro lo studente con le visite o manteniamo solo l'id dello studente?
+    //Perchè non includere time dentro day? (La classe DateTime contiene sia una data che un orario)
+    //Dobbiamo avere un array dentro lo studente con le visite o manteniamo solo l'id dello studente?
     private int $idAccommodation;
 }
