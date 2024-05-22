dee8c3d9e1dc4ce1442456a6761ed483c4639109
Elisabetta Mecozzi <163145808+elisamec@users.noreply.github.com>
2024-05-22 11:39:40 +0200

    EOwner.php restoration

---
+++ Entity/EOwner.php
@@ -0,0 +1,15 @@
+<?php
+
+class EOwner
+{
+    private int $idOwner;
+    private string $username;
+    private string $password;
+    private string $name;
+    private string $surname;
+    private EPhoto $photo;
+    private string $email;
+    private int $phoneNumber;
+    private string $iban;
+
+}
