c99e2a945015a3e6325eee90736609f097a68aac
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-05-25 11:06:08 +0200

    Prima versione FCreditCard

--- Foundation/FCreditCard.php
+++ Foundation/FCreditCard.php
@@ -2,12 +2,25 @@
 
 require_once ('FConnection.php');
 require_once ('../Entity/ECreditCard.php');
+/**
+ * FCreditCard
+ */
 class FCreditCard
 {
     private static $instance=null;
-
+    
+    /**
+     * __construct
+     *
+     * @return self
+     */
     public function __construct(){}
-
+    
+    /**
+     * getInstance
+     *
+     * @return FCreditCard
+     */
     public static function getInstance():FCreditCard
     {
         if(is_null(self::$instance))
@@ -16,7 +29,13 @@ class FCreditCard
         }
         return self::$instance;
     }
-
+    
+    /**
+     * exist
+     *
+     * @param  int $number
+     * @return bool
+     */
     public function exist(int $number):bool 
     {
         $q='SELECT * FROM owner WHERE number=:number';
@@ -36,7 +55,13 @@ class FCreditCard
         return false;
 
     }
-
+    
+    /**
+     * load
+     *
+     * @param  int $number
+     * @return ECreditCard
+     */
     public function load(int $number): ECreditCard 
     {
         $db=FConnection::getInstance()->getConnection();
@@ -59,12 +84,106 @@ class FCreditCard
         $result=new ECreditCard($row['number'],$row['name'],$row['surname'],$row['expiry'],$row['cvv'],$row['studentId']);
         return $result;
     }
+  
+  /**
+   * store
+   *
+   * @param  ECreditCard $CreditCard
+   * @return bool
+   */
+  public function store(ECreditCard $CreditCard):bool 
+  {
+    $db=FConnection::getInstance()->getConnection();
+    $db->setAttribute(PDO::ATTR_AUTOCOMMIT,false);
+    $db->beginTransaction();
+    try
+    {
+        $q='INSERT INTO creditcard (number, name , surname, expiry, cvv, studentId)';
+        $q.=' VALUES (:number, :name, :surname, :expiry, :cvv, :studentID)';
+        $db->exec('LOCK TABLES creditcard WRITE');
+        $stm=$db->prepare($q);
+        $stm->bindValue(':number',$CreditCard->getNumber(),PDO::PARAM_INT);
+        $stm->bindValue(':name',$CreditCard->getName(),PDO::PARAM_STR);
+        $stm->bindValue(':surname',$CreditCard->getSurname(),PDO::PARAM_STR);
+        $stm->bindValue(':expiry',$CreditCard->getExpiry(),PDO::PARAM_STR);
+        $stm->bindValue(':cvv',$CreditCard->getCVV(),PDO::PARAM_INT);
+        $stm->bindValue(':studentId',$CreditCard->getStudentID(),PDO::PARAM_INT);
+        $stm->execute();
+        $db->exec('UNLOCK TABLES');
+        $db->commit();
+        return true;
+    }
+    catch(PDOException $e)
+    {
+        $db->rollBack();
+        return false;
+    }
 
- #   public function store(EOwner $owner):bool {}
+  }
+    
+    /**
+     * update
+     *
+     * @param  ECreditCard $CreditCard
+     * @return bool
+     */
+    public function update(ECreditCard $CreditCard):bool 
+    {
+        $db=FConnection::getInstance()->getConnection();
+        $db->setAttribute(PDO::ATTR_AUTOCOMMIT,false);
+        $db->beginTransaction();
+        try
+        {
+            $q='UPDATE creditcard SET name = :name, surname = :surname, expiry = :expiry, cvv = :cvv, studentId = :studentId  WHERE number=:number';
+            $db->exec('LOCK TABLES creditcard WRITE');
+            $stm=$db->prepare($q);
+            $stm->bindValue(':name',$CreditCard->getName(),PDO::PARAM_STR);
+            $stm->bindValue(':surname',$CreditCard->getSurname(),PDO::PARAM_STR);
+            $stm->bindValue(':expiry',$CreditCard->getExpiry(),PDO::PARAM_STR);
+            $stm->bindValue(':cvv',$CreditCard->getCVV(),PDO::PARAM_INT);
+            $stm->bindValue(':studentId',$CreditCard->getStudentID(),PDO::PARAM_INT);
+            $stm->bindValue(':number',$CreditCard->getNumber(),PDO::PARAM_INT);
+            $stm->execute();
+            $db->exec('UNLOCK TABLES');
+            $db->commit();
+            return true;
+        }
+        catch(PDOException $e)
+        {
+            $db->rollBack();
+            return false;
+        }
 
- #   public function update(EOwner $owner):bool {}
-
- #   public function delete(int $id): bool {}
+    }
+    
+    /**
+     * delete
+     *
+     * @param  int $number
+     * @return bool
+     */
+    public function delete(int $number): bool 
+    {
+        $db=FConnection::getInstance()->getConnection();
+        $db->setAttribute(PDO::ATTR_AUTOCOMMIT,false);
+        $db->beginTransaction();
+        try
+        {
+            $q='DELETE FROM creditcard WHERE number= :number';
+            $db->exec('BLOCK TABLES creditcard WRITE');
+            $stm=$db->prepare($q);
+            $stm->bindValue(':number',$number, PDO::PARAM_INT);
+            $stm->execute();
+            $db->exec('UNLOCK TABLES');
+            $db->commit();
+            return true;
+        }
+        catch(PDOException $e)
+        {
+            $db->rollBack();
+            return false;
+        }
+    }
 
 
 }
