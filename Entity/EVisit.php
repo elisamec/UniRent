e23533ae2fb62af6c842f3da84cc7fa574269746
Nadia Muzyka <muzykanadia0@gmail.com>
2024-05-26 13:22:21 +0200

    Aggiunti  metodi get e set

--- Entity/EVisit.php
+++ Entity/EVisit.php
@@ -3,8 +3,43 @@
 class EVisit
 {
     private int $idVisit;
-    private DateTime $day;
-    //Perchè non includere time dentro day? (La classe DateTime contiene sia una data che un orario)
-    //Dobbiamo avere un array dentro lo studente con le visite o manteniamo solo l'id dello studente?
+    private DateTime $date;
+    private int $idStudent;
     private int $idAccommodation;
+
+    private static $entity = EVisit::class;
+
+    public function __construct(int $idVisit, DateTime $date, int $idStudent, int $idAccommodation){
+        $this->idVisit=$idVisit;
+        $this->date=$date;
+        $this->idStudent=$idStudent;
+        $this->idAccommodation=$idAccommodation;
+    }
+
+    public function getEntity():string {
+        return $this->entity;
+    }
+
+    public function getIdVisit():int {
+        return $this->idVisit;
+    }
+
+    public function getDate():DateTime{
+        return $this->date;
+    }
+
+    public function getIdStudent():int {
+        return $this->idStudent;
+    }
+
+    public function getIdAccommodation():int {
+        return $this->idAccommodation;
+    }
+
+    public function setDate(DateTime $date) {
+        $this->date = $date;
+    }
+
+
+
 }
