f9e739cc8ad709e525366fc17f5917ef692d698f
Nadia Muzyka <muzykanadia0@gmail.com>
2024-05-28 14:21:00 +0200

    prova

--- Classes/Foundation/indexNadia.php
+++ Classes/Foundation/indexNadia.php
@@ -2,6 +2,8 @@
 
     #PARTE NADIA
 
+    require __DIR__ . '../../Tools/composer/vendor/autoload.php';
+
     require_once ('FVisit.php');
     require_once ('../Entity/EVisit.php');  
 
@@ -25,7 +27,7 @@
     //$date = new DateTime("2002-09-15");
     //$studente= new EStudent(2, "Pippo", "pass", "Pippo", "Pluto", 1, "pippo@pluto.it", 3, 2032, $date, false, false, false);
 
-    $visit = new EVisit(0, "2024-09-15", 1, 2);
+    /*$visit = new EVisit(0, "2024-09-15", 1, 2);
     $esito=$FV->store($visit);
     if($esito==true)
     {
@@ -39,6 +41,20 @@
     else
     {
         echo 'Qualcosa non và! ';
-    }
+    }*/
     
+    
+
+    use CommerceGuys\Addressing\Address;
+    use CommerceGuys\Addressing\AddressFormat\AddressFormatRepository;
+
+    // Creazione di un oggetto Address
+    $address = new Address();
+    $address = $address->withCountryCode('US')
+                    ->withAdministrativeArea('CA')
+                    ->withLocality('Los Angeles')
+                    ->withPostalCode('90001')
+                    ->withAddressLine1('123 Main St');
+
+    echo $address->getCountryCode(); // Output: US
     
