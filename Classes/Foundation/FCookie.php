48fcf582fd4785eafc49efcef1f061e414270a86
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-06-21 18:16:23 +0200

    Aggiunto FCookie

---
+++ Classes/Foundation/FCookie.php
@@ -0,0 +1,20 @@
+<?php
+namespace Classes\Control;
+/**
+ * class to access to $_COOKIE superglobal array, You must use this class and not directly the _COOKIE array
+ */
+class FCookie
+{
+    /**
+     * check if is set the specific id in the COOKIE
+     * @return bool
+     */
+    public static function isSet($id){
+        if (isset($_COOKIE[$id])){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
