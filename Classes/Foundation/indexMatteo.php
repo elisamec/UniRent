373682cccf8fbad579a35025e284388cb34b728e
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-06-22 20:14:37 +0200

    Aggiunta PHPMailer

--- Classes/Foundation/indexMatteo.php
+++ Classes/Foundation/indexMatteo.php
@@ -1,7 +1,7 @@
 <?php 
 
 # PARTE MATTEO cancellato materiale precedente 22/06/2024
-
+/*
 $path='UniIta.json';
 $json=file_get_contents($path);
 $myarray=json_decode($json,true);
@@ -19,3 +19,28 @@ foreach($università as $key=>$value)
     }   
 }
 print_r($indirizzi_web);
+*/
+
+//Multiple recipients
+$to='matteo.maloni.00@gmail.com';
+//Subject
+$subject='oggetto della email';
+//Message
+$message='
+<html>
+<head>
+ <title>Esempio email in formato html</title>
+</head>
+<body>
+<p>Ciao Matteo!!!!</p>
+</body>
+</html>
+';
+//To send HTML mail, the Content-type header must be set
+$headers[]='MIME-Version:1.0';
+$headers[]='Content-type:text/html; charset=iso-8859-1';
+$headers[]='From:matteo.maloni.00@gmail.com';
+$headers[]='Cc:matteo.maloni.00@gmail.com';
+$headers[]='matteo.maloni.00@gmail.com';
+// Mail it
+mail($to,$subject,$message,implode("\r\n", $headers));
