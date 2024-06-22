63741fb623b5862b2c370c1a5a22fa666fc344bf
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-06-22 12:45:08 +0200

    Registration CUser

--- Classes/Control/CUser.php
+++ Classes/Control/CUser.php
@@ -2,9 +2,14 @@
 
 namespace Classes\Control;
 require __DIR__.'/../../vendor/autoload.php';
-use Classes\View\VUser; 
 
-class CUser{
+use Classes\Foundation\FPersistentManager;
+use Classes\Utilities\USession;
+use Classes\View\VUser; 
+use Classes\Utilities\USuperGlobalAccess;
+
+class CUser
+{
     
     public static function home(){
         $view = new VUser();
@@ -14,5 +19,33 @@ class CUser{
     public static function login(){
         $view = new VUser();
         $view->login();
+    }
+
+    public static function registration()
+    {
+        $view= new VUser();
+        $PM=FPersistentManager::getInstance();
+        if($PM->verifyUserEmail(USuperGlobalAccess::getPost('email')) && $PM->verifyUserUsername(USuperGlobalAccess::getPost('username')))
+        {
+            $session=USession::getInstance();
+            $session::setSessionElement('email',USuperGlobalAccess::getPost('email'));
+            $session::setSessionElement('username',USuperGlobalAccess::getPost('username'));
+            $session::setSessionElement('password',USuperGlobalAccess::getPost('password'));
+            $session::setSessionElement('name',USuperGlobalAccess::getPost('name'));
+            $session::setSessionElement('surname',USuperGlobalAccess::getPost('surname'));
+            $session::setSessionElement('picture',serialize(USuperGlobalAccess::getPost('picture')));
+            if(USuperGlobalAccess::getPost('studente/owner')==='student')
+            {
+               $view->showStudentRegistration();
+            }
+            else
+            {
+                $view->showOwnerRegistration();
+            }
+        }  
+        else
+        {
+            $view->reggistrationError();
+        }   
     }
 }
