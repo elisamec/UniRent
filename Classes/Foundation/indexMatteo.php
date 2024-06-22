6173a333585a5dbe88424fae52a543c51bc4bd77
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-06-22 16:37:37 +0200

    Aggiunta classe UAccessUniversityFile

--- Classes/Foundation/indexMatteo.php
+++ Classes/Foundation/indexMatteo.php
@@ -1,137 +1,21 @@
 <?php 
 
-    #PARTE MATTEO
-/*
+# PARTE MATTEO cancellato materiale precedente 22/06/2024
 
-PROVA STUDENT
+$path='UniIta.json';
+$json=file_get_contents($path);
+$myarray=json_decode($json,true);
 
-require_once('../Entity/EStudent.php');
-require_once('FStudent.php');
-
-$FS=FStudent::getInstance();
-$student=$FS->load(3);
-echo $student->getName().' '.$student->getSurname();
-$student->setSurname('Maloni');
-
-
-
-$risultato=$FS->update($student);
-
-if($risultato!=false)
+print_r($myarray['records'][0][12]);
+$università=$myarray['records'];
+$indirizzi_web=array();
+foreach($università as $key=>$value)
 {
-    echo 'Studente modificato nel db!';
-}
-else
-{
-    echo 'Qualcosa non và!';
-}
-*/
-
-
-#PROVA ADMINISTRATOR
-
-
-/*
-require_once('../Entity/EAdministrator.php');
-require_once('FAdministrator.php');
-
-$id=2;
-$FA=FAdministrator::getInstance();
-$amministratore=$FA->load($id);
-if($amministratore==false)
-{
-    echo 'Questo amministratore non esiste! ';
-}
-else
-{
-    echo $amministratore->__toString();
-}
-*/
-/*
-$FA=FAdministrator::getInstance();
-$admin=$FA->load(1);
-if($admin==false)
-{
-    echo 'NON ESISTE!';
-}
-else
-{
-    echo $admin->__toString();
-    $result=$FA->delete($admin->getID());
-    if($result==true)
+    $domain = substr(strrchr($università[$key][12], "www"), 1);
+    $domain ='@student'.$domain;
+    if($domain!='@student')
     {
-        echo 'Tutto ok !';
-    }
-    else
-    {
-        echo 'Qualcosa non và';
-    }
+        $indirizzi_web[]=$domain;
+    }   
 }
-*/
-
-# PROVA FReservation
-/*
-require_once('FReservation.php');
-require_once('../Entity/EReservation.php');
-
-$FR=FReservation::getInstance();
-$result=$FR->getWaitingReservations(2);
-print_r($result);
-
-*/
-/*
-
-if($result===true)
-{
-    echo 'Reservation effettuata!';
-}
-else
-{
-    echo 'Qualcosa no và!';
-}
-
-
-/*
-# PROVA EXIST FCONTRACT
-
-require_once('FContract.php');
-
-$FC= FContract::getInstance();
-$id=3;
-$result=$FC->exist($id);
-
-if($result)
-{
-    echo 'Il contratto esiste!';
-}
-else
-{
-    echo 'Il contratto non esiste!';
-}
-
-*/
-
-# prova uso di address
-/*
-require __DIR__ . '/../../vendor/autoload.php';
-use CommerceGuys\Addressing\Address;
-$address=new Address('Italy');
-echo $address->getCountryCode();
-*/
-require __DIR__.'../../../vendor/autoload.php';
-use Classes\Foundation\FPersistentManager;
-
-use Classes\Entity\EStudent;
-
-$BIRTH=new DateTime('12/12/1269');
-$PM= FPersistentManager::getInstance();
-$student= new EStudent('Dante','DivinaCommedia','Dante','Alighieri',null,'danteAlighieri@gmail.com',3,1300,$BIRTH,'M',false,false);
-$result=$PM::store($student);
-if($result)
-{
-    echo 'Tutto ok!';
-}
-else
-{
-    echo 'Qualcosa non và!';
-}
+print_r($indirizzi_web);
---
+++ Classes/Utilities/UAccessUniversityFile.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace Classes\Utilities;
+
+class UAccessUniversityFile
+{
+    public static $instance= null;
+    private static $list=array();
+
+    private function __construct()
+    {
+        $path='UniIta.json';
+        $json=file_get_contents($path);
+        $myarray=json_decode($json,true);
+        $università=$myarray['records'];
+        $indirizzi_web=array();
+        foreach($università as $key=>$value)
+        {
+            $domain = substr(strrchr($università[$key][12], "www"), 1);
+            $domain ='@student'.$domain;
+            if($domain!='@student')
+            {
+                $indirizzi_web[]=$domain;
+            }   
+        }
+        self::$list=$indirizzi_web;
+    }
+
+    public static function getInstance():UAccessUniversityFile
+    {
+        if(is_null(self::$instance))
+        {
+            self::$instance= new UAccessUniversityFile();
+        }
+        return self::$instance;
+    }
+
+    public function getUniversityEmailList():array
+    {
+        $result=self::$list;
+        return $result;
+    }
+    public function close():void
+    {
+        self::$instance=null;
+    }
+}
