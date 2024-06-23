7354678ab246cb489dc7056ca95714cf520b9169
Nadia Muzyka <muzykanadia0@gmail.com>
2024-06-23 10:48:42 +0200

    debbug

--- Classes/Utilities/USession.php
+++ Classes/Utilities/USession.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Classes\Utilities;
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__ . '/../../config.php';
+
 /**
  * class to access to the $_SESSION superglobal array, you Must use this class instead of using directly the array
  */
@@ -15,8 +18,8 @@ class USession
 
     private function __construct() 
     {
-        session_set_cookie_params(2592000); //set the duration of the session cookie
-        //session_set_cookie_params(COOKIE_EXP_TIME); //set the duration of the session cookie
+        //session_set_cookie_params(2592000); //set the duration of the session cookie
+        session_set_cookie_params(COOKIE_EXP_TIME); //set the duration of the session cookie
         session_start(); //start the session
     }
  
--- Classes/View/VOwner.php
+++ Classes/View/VOwner.php
@@ -2,7 +2,6 @@
 
 namespace Classes\View;
 require __DIR__.'/../../vendor/autoload.php';
-
 use StartSmarty;
 
 class VOwner {
