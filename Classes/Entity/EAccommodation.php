bde7476237ba7d17e171ce5fadbf5bb2d1e69948
Matteo Maloni <matteo.maloni.00@gmail.com>
2024-07-03 20:35:00 +0200

    commit visits

--- Classes/Entity/EAccommodation.php
+++ Classes/Entity/EAccommodation.php
@@ -439,10 +439,16 @@ class EAccommodation
     {
         $result=array();
         $myarray=json_decode($json,true);
-        print_r($myarray);
+        #print_r($myarray);
+        $result=array();
         foreach($myarray as $key=>$value)
         {
             $difference=date_diff(date_create($value['start']),date_create($value['end']));
+            $totalMinutes = $difference->h * 60 + $difference->i;
+            $value['diff']=(int)$totalMinutes;
+            $value['number_of_visits']=$value['diff']/(int)$value['duration'];
+            $result[$key]=$value;
         }
-  
+        print_r($result);
     }
+}
