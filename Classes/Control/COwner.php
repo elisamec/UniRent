771f329e1bc3e02407b1ec67e2c9f85e9426304d
Elisabetta Mecozzi <elisabetta.mecozzi0@gmail.com>
2024-07-17 10:06:56 +0200

    Refactor COwner.php to use variables for disabled and deletable flags
    This commit refactors the COwner.php file to use variables for the disabled and deletable flags in the `accommodationManagement` method. Instead of hardcoding `true` and `false`, the `disabled` and `deletable` variables are introduced and passed as arguments to the method. This change improves code readability and maintainability.

--- Classes/Control/COwner.php
+++ Classes/Control/COwner.php
@@ -126,7 +126,9 @@ class COwner
                 ];
             }
         }
-        $view->accommodationManagement($accomm, $owner, $reviewsData, $picture, $tenants, $num_places, true, false);
+        $disabled=true;
+        $deletable=false;
+        $view->accommodationManagement($accomm, $owner, $reviewsData, $picture, $tenants, $num_places, $disabled, $deletable);
     }
 
     public static function ownerRegistration(){
