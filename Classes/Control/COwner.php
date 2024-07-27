b84e34c144cb3cd6df5a5900116694797a129572
Nadia Muzyka <muzykanadia0@gmail.com>
2024-07-27 11:25:23 +0200

    owner

--- Classes/Control/COwner.php
+++ Classes/Control/COwner.php
@@ -14,7 +14,7 @@ use Classes\Entity\EPhoto;
 use Classes\Entity\EStudent;
 use Classes\Foundation\FOwner;
 use Classes\Foundation\FReview;
-use Classes\Tools\TStatusUser\TStatusUser;
+use Classes\Tools\TStatusUser;
 use Classes\Tools\TType;
 use CommerceGuys\Addressing\Address;
 use DateTime;
