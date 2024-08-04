d987fcd91ad0e750f33c9722df33e28a692c9738
Elisabetta Mecozzi <elisabetta.mecozzi0@gmail.com>
2024-08-04 15:04:37 +0200

    Refactor Smarty configuration paths for improved organization

--- Configuration/StartSmarty.php
+++ Configuration/StartSmarty.php
@@ -5,10 +5,10 @@ require(__DIR__ . '/../Smarty/Smarty.class.php');
 class StartSmarty{
     static function configuration(){
         $smarty=new Smarty();
-        $smarty->template_dir= __DIR__ . '/Smarty/templates/';
-        $smarty->compile_dir= __DIR__ . '/Smarty/templates_c/';
-        $smarty->config_dir= __DIR__ . '/Smarty/configs/';
-        $smarty->cache_dir= __DIR__ . '/Smarty/cache/';
+        $smarty->template_dir= __DIR__ . '/../Smarty/templates/';
+        $smarty->compile_dir= __DIR__ . '/../Smarty/templates_c/';
+        $smarty->config_dir= __DIR__ . '/../Smarty/configs/';
+        $smarty->cache_dir= __DIR__ . '/../Smarty/cache/';
         return $smarty;
     }
 }
